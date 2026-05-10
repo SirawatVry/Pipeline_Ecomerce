61c5dd0022d452843f184139c06bab3005f01f16
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2026-05-10 13:04:41 +0700

    change ignore

--- dbt_ecommerce/models/ecom_sales_by_country.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    country,
-    COUNT(DISTINCT invoice_no)          AS total_orders,
-    COUNT(DISTINCT customer_id)         AS unique_customers,
-    ROUND(SUM(total_price)::numeric, 2) AS total_revenue
-FROM {{ ref('stg_ecom') }}
-GROUP BY country
-ORDER BY total_revenue DESC
